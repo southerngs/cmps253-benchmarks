f2f82d1171f563b998cba03cbbda1b871b77b4d5
Gabriel Southern <gsouther@soe.ucsc.edu>
2014-05-28 15:44:27 -0700

    add option for full/test for v8

--- runs/v8-bench.sh
+++ runs/v8-bench.sh
@@ -25,13 +25,59 @@ runbench_input() {
   eval "$TIMECMD $JSBIN $BENCHCMD < $PARAM > $LOG" 
 }
 
-# All bench with smallest input
-runbench_param "binarytrees" "12"
-runbench_param "fannkuchredux" "10"
-runbench_param "fasta" "250000"
-runbench_input "knucleotide" "250000"
-runbench_param "nbody" "500000"
-runbench_input "regexdna" "50000"
-runbench_param "spectralnorm" "500"
+if [ "$4" != "full" ]
+then
+  # All bench with smallest input
+  runbench_param "binarytrees" "12"
+  runbench_param "fannkuchredux" "10"
+  runbench_param "fasta" "250000"
+  runbench_input "knucleotide" "250000"
+  runbench_param "nbody" "500000"
+  runbench_input "regexdna" "50000"
+  runbench_param "spectralnorm" "500"
+else
+  #binarytrees
+  runbench_param "binarytrees" "12"
+  runbench_param "binarytrees" "12"
+  runbench_param "binarytrees" "16"
+  runbench_param "binarytrees" "20"
 
+  #fannkuchredux
+  runbench_param "fannkuchredux" "10"
+  runbench_param "fannkuchredux" "10"
+  runbench_param "fannkuchredux" "11"
+  runbench_param "fannkuchredux" "12"
+
+  #fasta
+  runbench_param "fasta" "250000"
+  runbench_param "fasta" "250000"
+  runbench_param "fasta" "2500000"
+  runbench_param "fasta" "25000000"
+
+  #knucleotide
+  runbench_input "knucleotide" "250000"
+  runbench_input "knucleotide" "250000"
+  runbench_input "knucleotide" "2500000"
+  runbench_input "knucleotide" "25000000"
+
+  #nbody
+  runbench_param "nbody" "500000"
+  runbench_param "nbody" "500000"
+  runbench_param "nbody" "5000000"
+  runbench_param "nbody" "50000000"
+
+  #regexdan
+  runbench_input "regexdna" "50000"
+  runbench_input "regexdna" "50000"
+  runbench_input "regexdna" "500000"
+  runbench_input "regexdna" "5000000"
+
+  #Note that revcomp is too slow to run with Rubnius right now
+
+  #spectralnorm
+  runbench_param "spectralnorm" "500"
+  runbench_param "spectralnorm" "500"
+  runbench_param "spectralnorm" "3000"
+  runbench_param "spectralnorm" "5500"
+fi
 
