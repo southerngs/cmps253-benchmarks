3959acb80735c993fe2c4285dcdebc45811de226
Gabriel Southern <gsouther@soe.ucsc.edu>
2014-05-28 15:00:07 -0700

    adding knucleotide for javascript

--- runs/moz-bench.sh
+++ runs/moz-bench.sh
@@ -17,19 +17,19 @@ runbench_param() {
   eval "$TIMECMD $JSBIN $BENCHCMD $PARAM > $LOG" 
 }
 
-#runbench_input() {
-#  LOG=$1-$2.log
-#  BENCHCMD="$BENCH_ROOT/$1/$1.rb"
-#  PARAM=$INPUTS_DIR/fasta$2.txt
-#  TIMECMD="/usr/bin/time -f '$1-$2: %e sec (%C)'"
-#  eval "$TIMECMD $RUBYBIN $BENCHCMD $PARAM $LOG" 
-#}
+runbench_input() {
+  LOG=$1-$2.log
+  BENCHCMD="$BENCH_ROOT/$1/$1-moz.js"
+  PARAM=$INPUTS_DIR/fasta$2.txt
+  TIMECMD="/usr/bin/time -f '$1-$2: %e sec (%C)'"
+  eval "$TIMECMD $JSBIN $BENCHCMD < $PARAM > $LOG" 
+}
 
 # All bench with smallest input
 runbench_param "binarytrees" "12"
 runbench_param "fannkuchredux" "10"
 runbench_param "fasta" "250000"
-#runbench_input "knucleotide" "250000"
+runbench_input "knucleotide" "250000"
 #runbench_param "nbody" "500000"
 #runbench_input "regexdna" "50000"
 #runbench_param "spectralnorm" "500"
