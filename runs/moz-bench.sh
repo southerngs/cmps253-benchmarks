73057cfa59643ef4e45b8a359bed7f3cf84234f7
Gabriel Southern <gsouther@soe.ucsc.edu>
2014-05-28 12:07:51 -0700

    adding script for running Mozilla JS benchmarks

---
+++ runs/moz-bench.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ "$#" -lt "3" ]; then
+  echo "Usage ./moz-bench.sh <moz-js_bin> <inputs_dir> <bench_root>"
+  exit
+fi
+
+JSBIN=$1
+INPUTS_DIR=$2
+BENCH_ROOT=$3
+
+runbench_param() {
+  BENCHCMD="$BENCH_ROOT/$1/$1-moz.js"
+  PARAM=$2
+  TIMECMD="/usr/bin/time -f '$1-$2: %e sec (%C)'"
+  eval "$TIMECMD $JSBIN $BENCHCMD $PARAM" 
+}
+
+#runbench_input() {
+#  LOG=$1-$2.log
+#  BENCHCMD="$BENCH_ROOT/$1/$1.rb"
+#  PARAM=$INPUTS_DIR/fasta$2.txt
+#  TIMECMD="/usr/bin/time -f '$1-$2: %e sec (%C)'"
+#  eval "$TIMECMD $RUBYBIN $BENCHCMD $PARAM $LOG" 
+#}
+
+# All bench with smallest input
+runbench_param "binarytrees" "12"
+#runbench_param "fannkuchredux" "10"
+#runbench_param "fasta" "250000"
+#runbench_input "knucleotide" "250000"
+#runbench_param "nbody" "500000"
+#runbench_input "regexdna" "50000"
+#runbench_param "spectralnorm" "500"
+
+
